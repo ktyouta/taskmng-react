da72728c879dfc1a0618eb8b89336a64867b6a2f
ktyouta <ktyouta0506@gmail.com>
2024-10-13 13:32:59 +0900

    NotFoundコンポーネントの修正

--- src/NotFound/NotFoundComponent.tsx
+++ src/NotFound/NotFoundComponent.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
 import { Link } from "react-router-dom";
-import './css/NotFoundComponent.css';
+import styled from 'styled-components';
 
 //引数の型
 type porpType = {
-    backUrl: string
+    backUrl?: string,
+    style?: { [key: string]: string }
 }
+
+//外側のスタイル
+const OuterDiv = styled.div`
+    box-sizing: border-box;
+    padding-left: 8%;
+    padding-top: 4%;
+`;
 
 function NotFoundComponent(props: porpType) {
     return (
-        <div className="notfound">
+        <OuterDiv
+            style={props.style}
+        >
             <h1>404 NOT FOUND</h1>
             <p>お探しのページが見つかりませんでした。</p>
-            <Link to={props.backUrl}>Topに戻る</Link>
-        </div>
+            {
+                props.backUrl &&
+                <Link to={props.backUrl}>Topに戻る</Link>
+            }
+        </OuterDiv>
     )
 }
 
